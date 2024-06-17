3faacb76966c31095e8da264adf164376b7cfa3a
LeisyVasquez <leisyyvalentina@gmail.com>
2024-06-17 17:28:38 -0500

    delete console.log

--- src/utilities/addCardToDeck.js
+++ src/utilities/addCardToDeck.js
@@ -2,27 +2,18 @@ import { valueCards } from './constants';
 import { validationWin } from './validationWin';
 
 const addCardToPlayer = (cardsWasteBefore, cardsWasteAfter, newCard, cards) => {
-	console.log(
-		'Longitud 1',
-		cardsWasteBefore.length,
-		'Longitud 2',
-		cardsWasteAfter.length,
-	);
 	if (cardsWasteBefore.length + 1 !== cardsWasteAfter.length) {
-		console.log('Holi 1');
 		cards.push(newCard);
 		const cardDelete = cardsWasteAfter.pop();
 		const newCards = cards.filter(card => card.code !== cardDelete.code);
 		return { newCards, cardsWasteAfter };
 	} else {
-		console.log('Voy a buscar pares');
 		let isCoupleNumber = false;
 		let indexCoupleNumber = 0;
 
 		for (let i = 0; i < cardsWasteBefore.length; i++) {
 			const card = cardsWasteBefore[i];
 			if (card.value === newCard.value) {
-				console.log('Condición 1');
 				isCoupleNumber = true;
 				indexCoupleNumber = i;
 				break;
@@ -30,35 +21,21 @@ const addCardToPlayer = (cardsWasteBefore, cardsWasteAfter, newCard, cards) => {
 		}
 
 		if (isCoupleNumber) {
-			console.log('Condición 2');
 			const waste = cardsWasteBefore.filter(
 				(_, index) => index !== indexCoupleNumber,
 			);
-			console.log(waste);
 			const cardDelete = waste.pop();
-			console.log(waste);
-			console.log(cardDelete);
 			waste.push(newCard, cardsWasteBefore[indexCoupleNumber]);
-			console.log(waste);
 			const newCards = cards.filter(card => card.code !== cardDelete.code);
 			newCards.push(newCard);
-			console.log(newCards);
 			return { newCards, cardsWasteAfter: waste };
 		}
 
 		let isCoupleLadder = false;
 		let indexLadder = 0;
 
-		console.log('sobrantes', cardsWasteBefore);
 		for (let i = 0; i < cardsWasteBefore.length; i++) {
 			const card = cardsWasteBefore[i];
-			console.log(card.suit);
-			console.log(newCard.suit);
-			console.log(card.suit === newCard.suit);
-			console.log(valueCards[newCard.value] + 1);
-			console.log(card.value);
-			console.log(valueCards[newCard.value] - 1);
-
 			if (
 				card.suit === newCard.suit &&
 				(valueCards[newCard.value] + 1 === card.value ||
@@ -71,22 +48,13 @@ const addCardToPlayer = (cardsWasteBefore, cardsWasteAfter, newCard, cards) => {
 		}
 
 		if (isCoupleLadder) {
-			console.log('Sirvo para par de escalera');
 			const waste = cardsWasteBefore.filter(
 				(_, index) => index !== indexLadder,
 			);
-			console.log(waste);
 			const cardDelete = waste.pop();
-			console.log(waste);
-			console.log(cardDelete);
-
 			waste.push(newCard, cardsWasteBefore[indexLadder]);
-			console.log(waste);
-
 			const newCards = cards.filter(card => card.code !== cardDelete.code);
 			newCards.push(newCard);
-			console.log(newCards);
-
 			return { newCards, cardsWasteAfter: waste };
 		}
 	}
@@ -107,7 +75,6 @@ export const addCardToDecks = (
 ) => {
 	const newCardPlayerOne = newCards[0];
 	const newCardPlayerTwo = newCards[1];
-	console.log('Nuevas cartas', newCardPlayerOne, newCardPlayerTwo);
 	const {
 		cardsWastePlayerOne: cardsAfterWastePlayerOne,
 		cardsWastePlayerTwo: cardsAfterWastePlayerTwo,
@@ -117,8 +84,6 @@ export const addCardToDecks = (
 		[...cardsPlayerOne, newCardPlayerOne],
 		[...cardsPlayerTwo, newCardPlayerTwo],
 	);
-	console.log('1', cardsAfterWastePlayerOne);
-	console.log('2', cardsAfterWastePlayerTwo);
 
 	const {
 		newCards: newCardsPlayerOne,
